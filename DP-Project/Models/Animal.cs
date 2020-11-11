81402a003762831cc49926e603938171edd75fb3
Berni Cortvriend <bernicortvriend@Bernis-MacBook-Pro.local>
2020-11-11 12:48:26 +0100

    Aanmaken model push

---
+++ DP-Project/Models/Animal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace DPProject.Models
+{
+    public class Animal
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
+
+        [JsonProperty(PropertyName = "population")]
+        public string Population { get; set; }
+
+        [JsonProperty(PropertyName = "location")]
+        public string Location { get; set; }
+
+        [JsonProperty(PropertyName = "habitat")]
+        public string Habitat { get; set; }
+
+        public string Image { get; set; }
+
+        [JsonExtensionData]
+        private Dictionary<string, JToken> _extraJsonData = new Dictionary<string, JToken>();
+
+        [OnDeserialized]
+        private void ProcessExtraJsonData(StreamingContext context)
+        {
+            JToken prefsData = (JToken)_extraJsonData["image"];
+            Image = (string)prefsData.SelectToken("url");
+        }
+
+        public ImageSource ImaheSrc
+        {
+            get
+            {
+                return ImageSource.FromStream(() => new HttpClient().GetStreamAsync(Image).Result);
+            }
+        }
+
+
+    }
+}
